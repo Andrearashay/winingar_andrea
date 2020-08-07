6f4ad11f369dec45d529b375792ce86db3b9bdc4
Andrea Winingar <andrea.winingar@gmail.com>
2020-08-06 21:32:16 -0500

    Plot 1

--- hw15/winingar_hw15.R
+++ hw15/winingar_hw15.R
@@ -48,66 +48,61 @@ deaths_import
 confirmed_made_date_column <- confirmed_import %>% 
   pivot_longer(c(`1/22/20`:`7/31/20`), 
                names_to = 'date', 
-               values_to = 'confirmed')
+               values_to = 'confirmed') %>% 
+  mutate(date = dmy(date)) %>% 
+  filter(date <= dmy(first_us_case))
 confirmed_made_date_column
 
 deaths_made_date_column <- deaths_import %>% 
   pivot_longer(c(`1/22/20`:`7/31/20`),
                names_to = 'date',
-               values_to = 'deaths')
+               values_to = 'deaths') %>% 
+  mutate(date = dmy(date)) %>% 
+  filter(date <= dmy(first_us_case))
 deaths_made_date_column
 
-# Convert Date column to ISO 8601 format
-confirmed_date_format <- confirmed_made_date_column %>% 
-  mutate(date = mdy(date))
-confirmed_date_format
-
-deaths_date_format <- deaths_made_date_column %>% 
-  mutate(date = dmy(date))
-deaths_date_format
-
-# Remove dates before first US case
-confirmed_after_us_first <- confirmed_date_format %>% 
-  filter(date >= dmy(first_us_case))
-confirmed_after_us_first
-
-deaths_after_us_first <- deaths_date_format %>% 
-  filter(date >= dmy(first_us_case))
-deaths_after_us_first
 
 ### Plot 1
 
-# data set greater than or equal to first MO case
-covid_confirmed_and_deaths <- left_join(deaths_after_us_first, confirmed_after_us_first)
+# combined data
+covid_confirmed_and_deaths <- left_join(deaths_made_date_column,
+                                       confirmed_made_date_column)
 covid_confirmed_and_deaths
-covid_combined_mo <- covid_confirmed_and_deaths %>% 
-  filter(date >= mdy(first_mo_case))
-covid_combined_mo
 
 # I can't put the graphs side by side, could not download package to do so.
 
 # add regions to data
-covid_combined_regions <- covid_combined_mo %>% 
+covid_combined_regions <- covid_confirmed_and_deaths %>% 
+  filter(date >= mdy(first_mo_case)) %>% 
   mutate(regions = case_when(stateFIPS %in% northeast ~ "Northeast",
                              stateFIPS %in% south ~ "South",
                              stateFIPS %in% midwest ~ "Midwest",
-                             stateFIPS %in% west ~ "West"))
+                             stateFIPS %in% west ~ "West")) %>% 
+  group_by(regions, date) %>% 
+  summarise(total_cases = sum(confirmed,
+                              na.rm=TRUE),
+            total_deaths = sum(deaths,
+                               na.rm=TRUE),
+            .groups="drop")
 covid_combined_regions
 
-# Make total columns
-plot_one_data <- covid_combined_regions %>% 
-  summarize('Total Deaths' = sum(deaths, na.rm = TRUE),
-            'Total Confirmed' = sum(confirmed, na.rm = TRUE),
-            .groups = "drop")
-plot_one_data
-
 # plot 1- confirmed
-plot_one_data %>% 
+covid_combined_regions %>% 
   ggplot() +
-  geom_line(mapping = aes(x = date, y = 'Total Confirmed', color = 'regions'), size = 0.8) +
-  labs(x = NULL, y = "Total Cases")
+  geom_line(aes(x = date, y = total_cases, color = 'regions'), size = 0.8) +
+  labs(x = NULL, y = 'Total Cases') +
+  theme_test() +
+  theme(legend.position = 'bottom')
   
+# Plot 1- deaths
+covid_combined_regions %>% 
+  ggplot() +
+  geom_line(aes(x = date, y = total_deaths, color = 'regions'), size = 0.8) +
+  labs(x = NULL, y = "Total Deaths") +
+  theme_test() +
+  theme(legend.position = 'bottom')
 
+### Plot 2
 
 
 
